e54158359b7e5ad1b701441e4572aaad04f61908
alonso <alonso@wavegis.com.tw>
2014-07-07 14:34:43 +0800

    add csvhelper test

--- checkIfIp4Address/Program.cs
+++ checkIfIp4Address/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
-
+using CsvHelper;
+using CsvHelper.Configuration;
 namespace checkIfIp4Address
 {
     class Program
@@ -12,11 +14,20 @@ namespace checkIfIp4Address
         {
             while (true)
             {
-                string txt = Console.ReadLine();
+                using (StreamWriter sw = new StreamWriter(Environment.CurrentDirectory+"\\"+DateTime.Now.ToString("yy-MM-dd")+".csv",true))
+                {
+                    var csv = new CsvWriter(sw);
+                    csv.WriteField("1");
+                    csv.WriteField("2");
+                    csv.WriteField("3");
+                    csv.NextRecord();
+                    
+                }
+                string ip = Console.ReadLine();
                 string ValidIpAddressRegex = @"^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)";
                 Regex r = new Regex(ValidIpAddressRegex, RegexOptions.IgnoreCase | RegexOptions.Singleline);
    
-                Match m = r.Match(txt);
+                Match m = r.Match(ip);
                 if (m.Success)
                 {
                     Console.WriteLine(m.Value+" valid");
